9763011d544ba1b9111c95f4b5edab7671f258be
Benjamin Semah <benjaminsemah@gmail.com>
2022-07-04 16:55:10 +0000

    Fix issues with responsiveness

---
+++ index.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector(".hamburger");
+const body = document.querySelector("body");
+const navMenu = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-item");
+const header = document.querySelector("#header");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("hamburger-open");
+  navMenu.classList.toggle("open");
+
+  navLinks.forEach((navLinks) => {
+    navLinks.classList.toggle("fade");
+  });
+}
+
+navLinks.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("hamburger-open");
+  navMenu.classList.remove("open");
+
+  navLinks.forEach((navLink) => {
+    navLink.classList.remove("fade");
+  });
+}
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
