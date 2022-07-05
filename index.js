a097f628fac6d740dcacff32ddd2e4a4957229f9
Benjamin Semah <benjaminsemah@gmail.com>
2022-07-05 10:18:01 +0000

    Fix linter errors

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const hamburger = document.querySelector(".hamburger");
-const body = document.querySelector("body");
-const navMenu = document.querySelector(".nav-links");
-const navLinks = document.querySelectorAll(".nav-item");
-const header = document.querySelector("#header");
-
-hamburger.addEventListener("click", mobileMenu);
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-item');
+const header = document.querySelector('#header');
 
 function mobileMenu() {
-  hamburger.classList.toggle("hamburger-open");
-  navMenu.classList.toggle("open");
+  hamburger.classList.toggle('hamburger-open');
+  navMenu.classList.toggle('open');
 
   navLinks.forEach((navLinks) => {
-    navLinks.classList.toggle("fade");
+    navLinks.classList.toggle('fade');
   });
 }
-
-navLinks.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-  hamburger.classList.remove("hamburger-open");
-  navMenu.classList.remove("open");
+  hamburger.classList.remove('hamburger-open');
+  navMenu.classList.remove('open');
 
   navLinks.forEach((navLink) => {
-    navLink.classList.remove("fade");
+    navLink.classList.remove('fade');
   });
 }
 
-window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 0);
+hamburger.addEventListener('click', mobileMenu);
+navLinks.forEach((n) => n.addEventListener('click', closeMenu));
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('sticky', window.scrollY > 0);
 });
